a9437e5c725961424073c67dd06323928f09edf9
vini81576 <00001123235107sp@al.educacao.sp.gov.br>
2024-12-03 11:10:03 -0300

    adicionando arquivos do projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'esporte',
+    'No futsal o goleiro pode usar as mãos ?',
+    'Sim, apenas o goleiro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália ?',
+    'A capital da França é Roma'
+)
+
+criaCartao(
+    'Esporte',
+    'Quando foi a ultíma copa do mundo sediada no Brasil ?',
+    'Foi em 2014'
+)
+
+criaCartao(
+    'Filmes',
+    'Quem é o personagem(a) principal de Tropa de elite?',
+    'é o capitão nascimento'
+)
